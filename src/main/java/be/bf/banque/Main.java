f0c5c961c4c7e70292494eeaf5f526dc75b81f8a
PUTINATI Fabio <STG0365@BRUFOR.LAN>
2022-06-22 10:32:21 +0200

    Initial commit

---
+++ src/main/java/be/bf/banque/Main.java
@@ -0,0 +1,13 @@
+package be.bf.banque;
+
+
+import be.bf.banque.models.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Banque b = new Banque("Picsou");
+
+
+    }
+}
